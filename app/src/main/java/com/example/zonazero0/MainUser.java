94396388a194dcaea63366a5288e66ec94c33441
JulioF127 <juliofuentes12@hotmail.com>
2023-06-03 00:04:16 -0600

    Sistema de solicitudes mas o menos implementado

--- app/src/main/java/com/example/zonazero0/MainUser.java
+++ app/src/main/java/com/example/zonazero0/MainUser.java
@@ -7,6 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+
+import android.content.ContentValues;
+import com.example.zonazero0.DeviceManager;
+import com.example.zonazero0.Constantes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+import android.util.Log;
+import android.widget.Toast;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.icu.text.SimpleDateFormat;
@@ -14,14 +24,10 @@ import android.icu.util.TimeZone;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Base64;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -54,7 +60,7 @@ public class MainUser extends AppCompatActivity {
         btnLogout = findViewById(R.id.button);
         btnSolicitud = findViewById(R.id.button2);
         btnActualizar = findViewById(R.id.button5);
-
+        addDevice();
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -206,8 +212,34 @@ public class MainUser extends AppCompatActivity {
     }
 
 
+// Metodo para lo de firebase
+private void addDevice(){
+    FirebaseMessaging.getInstance().getToken()
+            .addOnCompleteListener(new OnCompleteListener<String>() {
+                @Override
+                public void onComplete(@NonNull Task<String> task) {
+                    if (!task.isSuccessful()) {
+                        Log.w(ContentValues.TAG,
+                                "Fetching FCM registration token failed",
+                                task.getException());
+                        return;
+                    }
 
+                    String token = task.getResult();
+                    String tokenGuardado = getSharedPreferences(Constantes.SP_FILE,0)
+                            .getString(Constantes.SP_KEY_DEVICEID,null);
+                    if (token != null ){
+                        if (tokenGuardado == null || !token.equals(tokenGuardado)){
+                            //registramos el token en el servidor
+                            DeviceManager.postRegistrarDispositivoEnServidor( token, MainUser.this);
+                        }
+                    }
 
+                    Toast.makeText(MainUser.this, token,
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+}
 
 
 }
